56b8c09be433481ce972e9b6a7b724d6e76eae64
Ben Firner <ben.firner@gmail.com>
2023-12-14 23:27:37 -0500

    More rSolver tests.

--- tests/test_arm_utility.py
+++ tests/test_arm_utility.py
@@ -8,11 +8,21 @@ import pytest
 import arm_utility
 import math
 
+testdata = [
+    ([0.23115182452192629, 0.0, 0.03520485992485018], 0.175),
+    ([0.3, 0.0, 0.03520485992485018], 0.175),
+    ([0.2, 0.0, 0.3], 0.175),
+    ([0.25, 0.0, 0.05],  0.3),
+    ([0.05249901188844186, 0.002786401510238651, 0.3881603860167609], 0.265), # this lead to [0.002786401510238651, -1.4469024488034017, -0.71, 1.6765656382911949, 0.004601942375302315], which is super high
+    ([0.25321934895550047, 0.0023725500898967374, 0.07310723154126952], 0.265), # this fails
+    ([0.19900299276422714, 0.006135923322290182, -0.0409016373742874], 0.265),
+]
 
-def test_rSolver():
+
+@pytest.mark.parametrize("target, hand_length", testdata, ids=["near", "far", "high", "long hand", "back failure", "near failure", "solver failure"])
+def test_rSolver(target, hand_length):
     # Attempt to generate a pose for a nearby target
-    target = [0.23115182452192629, 0.0, 0.03520485992485018]
-    segment_lengths = [0.104, 0.158, 0.147, 0.175]
+    segment_lengths = [0.104, 0.158, 0.147, hand_length]
     target_rtz = arm_utility.XYZToRThetaZ(*target)
     new_joints = arm_utility.rSolver(target_rtz[0], target_rtz[2], segment_lengths=segment_lengths)
 
@@ -21,50 +31,5 @@ def test_rSolver():
     assert abs(new_joints[2]) < math.pi/3.
 
     new_coords = arm_utility.computeGripperPosition([0.] + new_joints, segment_lengths)
-    distance = arm_utility.getDistance(new_coords, target)
-    assert distance < 0.001
-
-def test_rSolver():
-    # Attempt to generate a pose for a distant target
-    target = [0.3, 0.0, 0.03520485992485018]
-    segment_lengths = [0.104, 0.158, 0.147, 0.175]
-    target_rtz = arm_utility.XYZToRThetaZ(*target)
-    new_joints = arm_utility.rSolver(target_rtz[0], target_rtz[2], segment_lengths=segment_lengths)
-
-    assert abs(new_joints[0]) < math.pi/3.
-    assert abs(new_joints[1]) < math.pi/3.
-    assert abs(new_joints[2]) < math.pi/3.
-
-    new_coords = arm_utility.computeGripperPosition([0.] + new_joints, segment_lengths)
-    distance = arm_utility.getDistance(new_coords, target)
-    assert distance < 0.001
-
-def test_rSolver():
-    # Attempt to generate a pose for a high target
-    target = [0.2, 0.0, 0.3]
-    segment_lengths = [0.104, 0.158, 0.147, 0.175]
-    target_rtz = arm_utility.XYZToRThetaZ(*target)
-    new_joints = arm_utility.rSolver(target_rtz[0], target_rtz[2], segment_lengths=segment_lengths)
-
-    assert abs(new_joints[0]) < math.pi/3.
-    assert abs(new_joints[1]) < math.pi/3.
-    assert abs(new_joints[2]) < math.pi/3.
-
-    new_coords = arm_utility.computeGripperPosition([0.] + new_joints, segment_lengths=segment_lengths)
-    distance = arm_utility.getDistance(new_coords, target)
-    assert distance < 0.001
-
-def test_rSolver():
-    # Attempt to generate a pose for a near target with a longer gripper
-    target = [0.25, 0.0, 0.05]
-    segment_lengths = [0.104, 0.158, 0.147, 0.3]
-    target_rtz = arm_utility.XYZToRThetaZ(*target)
-    new_joints = arm_utility.rSolver(target_rtz[0], target_rtz[2], segment_lengths=segment_lengths)
-
-    assert abs(new_joints[0]) < math.pi/3.
-    assert abs(new_joints[1]) < math.pi/3.
-    assert abs(new_joints[2]) < math.pi/3.
-
-    new_coords = arm_utility.computeGripperPosition([0.] + new_joints, segment_lengths=segment_lengths)
     distance = arm_utility.getDistance(new_coords, target)
     assert distance < 0.001
